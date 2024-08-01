ba6fa3f71a45120ee76e402f3d4b2a76999778c2
MatveySviadysh <mkomp06@gmail.com>
2024-08-01 13:48:51 +0300

    update profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import Main from './components/Main';
+import Profile from './components/Profile';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/main" element={<Main />} />
+                <Route path="/profile" element={<Profile />} />
             </Routes>
         </Router>
     );
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,44 +1,10 @@
-// src/components/Profile.js
-import React, { useEffect, useState } from 'react';
-import { getProfile } from '../api';
+import React from 'react';
 
 const Profile = () => {
-    const [profile, setProfile] = useState(null);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        const fetchProfile = async () => {
-            try {
-                const token = localStorage.getItem('token'); // Убедитесь, что токен сохранен
-                console.log('Token:', token); // Проверка токена
-                if (token) {
-                    const profileData = await getProfile(token);
-                    setProfile(profileData);
-                } else {
-                    setError(new Error('Token not found'));
-                }
-            } catch (error) {
-                setError(error);
-            }
-        };
-
-        fetchProfile();
-    }, []);
-
-    if (error) {
-        return <div>Ошибка: {error.message}</div>;
-    }
-
-    if (!profile) {
-        return <div>Загрузка...</div>;
-    }
-
     return (
         <div>
-            <h1>Welcome, {profile.username}!</h1>
-            <p>Email: {profile.email}</p>
-            <p>First Name: {profile.first_name}</p>
-            <p>Last Name: {profile.last_name}</p>
+            <h1>Profile Page</h1>
+            <p>Welcome to your profile!</p>
         </div>
     );
 };
