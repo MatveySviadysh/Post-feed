9361554d829a945edb4355680d8c20a9f7a5714e
MatveySviadysh <mkomp06@gmail.com>
2024-08-06 19:27:51 +0300

    create component page

---
+++ frontend/src/components/Notification/NotificationList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h1>сдесь отображаються все уведомления </h1>
+            
+        </div>
+    );
+};
+
+export default Main;
