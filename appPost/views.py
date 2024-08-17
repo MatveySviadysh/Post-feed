7728683899ef390b3c9dc578be484fa48548832c
MatveySviadysh <mkomp06@gmail.com>
2024-08-17 12:24:03 +0300

    post line backend

--- appPost/views.py
+++ appPost/views.py
@@ -4,6 +4,7 @@ from .models import Post
 from .serializers import PostSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.permissions import AllowAny
+from rest_framework import viewsets
 
 class PostCreate(generics.ListCreateAPIView):
     queryset = Post.objects.all().order_by('-created_at')
@@ -23,3 +24,7 @@ class PostDetail(generics.RetrieveDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [AllowAny]
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
