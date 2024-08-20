2807b1bfa5fa6c613c285b8ed0c245ce425a256f
MatveySviadysh <mkomp06@gmail.com>
2024-08-20 13:58:54 +0300

    fix  port redis in settings

--- shatl/settings.py
+++ shatl/settings.py
@@ -170,8 +170,8 @@ APPEND_SLASH = False
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # Замените, если используете другой хост/порт
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
